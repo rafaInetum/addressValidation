3e7c63a8e505d47de0c6ac1d0d53ca1635012686
rafa2892 <rafav2009@gmail.com>
2025-08-14 12:53:44 +0200

    UPDATE: Delete no used parameter AddressValidationProps and Remove logs files from repo

--- src/main/java/org/correos/app/addressvalidation/infrastructure/google/config/GoogleAddressValidationProps.java
+++ src/main/java/org/correos/app/addressvalidation/infrastructure/google/config/GoogleAddressValidationProps.java
@@ -3,7 +3,4 @@ package org.correos.app.addressvalidation.infrastructure.google.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "google.address")
-public record GoogleAddressValidationProps(
-        String baseUrl,
-        String apiKey
-) {}
+public record GoogleAddressValidationProps(String baseUrl) {}
